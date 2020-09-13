4ba2e0447d121168e21967e9feb98e8f23f41d98
RachelM <rachieb19@gmail.com>
2020-09-13 01:43:49 -0400

    added files

---
+++ build/precache-manifest.8039bc62208f20f7e3cafb63527893c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5abcf89952eabd6bf9d7a23e299c584",
+    "url": "/react-news-app/index.html"
+  },
+  {
+    "revision": "05b1aab3902d88bfa9e4",
+    "url": "/react-news-app/static/css/main.5e228594.chunk.css"
+  },
+  {
+    "revision": "f68cf7580d58b8fc7fa2",
+    "url": "/react-news-app/static/js/2.aec3927a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-news-app/static/js/2.aec3927a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05b1aab3902d88bfa9e4",
+    "url": "/react-news-app/static/js/main.0d576452.chunk.js"
+  },
+  {
+    "revision": "8d61b2ea3e8db3334b89",
+    "url": "/react-news-app/static/js/runtime-main.6f7061ad.js"
+  }
+]);
